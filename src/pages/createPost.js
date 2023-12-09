f01a9f12d239b0e4b4955a97bdc7f3e3edccaf98
Kim Nguyen <kkn2011@nyu.edu>
2023-12-09 15:15:53 -0500

    update

--- src/pages/createPost.js
+++ src/pages/createPost.js
@@ -1,9 +1,10 @@
 import { useEffect } from "react"; 
-import { useRouter } from "next/router"; 
+import { useRouter } from "next/router";
+import Header from "@/app/components/Header";  
 import CreatePostForm from "@/app/components/CreatePostForm"
+import {getFirestore, collection, setDoc} from "firebase/firestore"; 
 
-
-export default function CreateUser ( { isLoggedIn } ) {
+export default function CreatePost ( { isLoggedIn, userInformation } ) {
 
     const router = useRouter(); 
     
@@ -11,14 +12,27 @@ export default function CreateUser ( { isLoggedIn } ) {
         if (!isLoggedIn) router.push("/"); 
     }, [isLoggedIn]); 
 
-    const createPostFunction = useCallback((e) => {
+    const createPostFunction = useCallback(
+        async(e) => {
         e.preventDefault(); 
         const postContent = e.currentTarget.postContent.value; 
-        console.log({postContent})
+        console.log({postContent}); 
 
 
+        const userId = userInformation.uid; 
 
-    }, [] ); 
+        const db = getFirestore();
+        await setDoc(doc(db, "posts"), {
+            postContent: postContent, 
+            usedId: userId
+        }); 
+
+        if(data) {
+            router.push("/"); 
+        }
+        
+
+    }, [addDoc, collection, getFirestore, userInformation, router] ); 
 
 
 
@@ -28,7 +42,7 @@ export default function CreateUser ( { isLoggedIn } ) {
 
         <main>
             <h1>Create Post</h1>
-            <CreatePostForm createPostFunction={createPostFunction}/> 
+            <CreatePostForm/> 
         </main>
 
         </>
